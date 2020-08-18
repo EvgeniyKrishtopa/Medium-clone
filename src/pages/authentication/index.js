7f91391d224cab555959c11c08386099ec6b1210
EvgeniyKrishtopa <evgeniykrishtopa@gmail.com>
2020-08-18 17:36:58 +0300

    finish login page

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Authentication = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [{ isLoading, error }, doFetch] = useFetch("users/login");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    doFetch({
+      method: "post",
+      data: {
+        user: {
+          email,
+          password,
+        },
+      },
+    });
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -9,28 +28,36 @@ const Authentication = () => {
           <div className="col-xs-12 col-md-6 offset-md-3">
             <h1 className="text-xs-center">Login</h1>
             <p className="text-xs-center">
-              <Link to="register">Need an account?</Link>
+              <Link to="/register">Need an account?</Link>
             </p>
-            <form>
+            <form onSubmit={handleSubmit}>
               <fieldset>
                 <fieldset className="form-group">
                   <input
                     type="email"
                     className="form-control form-control-lg"
                     placeholder="Email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
                   />
                 </fieldset>
                 <fieldset className="form-group">
                   <input
                     type="password"
                     className="form-control form-control-lg"
-                    placeholder="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
                   />
                 </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">
+                <button
+                  className="btn btn-lg btn-primary pull-xs-right"
+                  disabled={isLoading}
+                >
                   Sign in
                 </button>
               </fieldset>
+              {error && <p>Password or Login incorrected</p>}
             </form>
           </div>
         </div>
